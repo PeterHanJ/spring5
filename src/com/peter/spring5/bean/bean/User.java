b0c7430a6be29ba9f9c4bd86f40e24b387a71cd7
peter <peter_hanjun@hotmail.com>
2021-04-07 17:25:50 +0800

    commit

---
+++ src/com/peter/spring5/bean/bean/User.java
@@ -0,0 +1,41 @@
+package com.peter.spring5.bean.bean;
+
+public class User {
+    private String userName;
+    private String address;
+    private String age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", address='" + address + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
---
+++ test/com/peter/spring5/bean/TestUser.java
@@ -0,0 +1,15 @@
+package com.peter.spring5.bean;
+
+import com.peter.spring5.bean.bean.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUser {
+    @Test
+    public void testUser() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println("user:" + user.toString());
+    }
+}
